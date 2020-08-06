a047c52308baf56cd008470ef321c4ca1224ff2c
mohamedhany1024 <mhany4485@gmail.com>
2020-08-06 13:35:31 +0200

    Update 0.7 alpha

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import platform, subprocess, re
+import platform, subprocess, re, os
 
 app = Flask(__name__)
 
@@ -16,12 +16,23 @@ def home():
 @app.route('/settings')
 
 def settings():
+	memBytes = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+	memGig = 0
+	x = 0
+	while x in range(7):
+		memGig = memBytes/1024
+		memGig = memGig/1024
+		memGig = memGig/1024
+		memGig = round(memGig, 1)
+		
+		x += 1;
+		print(memGig)
 	command = "cat /proc/cpuinfo"
 	returnedValue = subprocess.check_output(command, shell=True).strip().decode()
 	for line in returnedValue.split("\n"):
 		if "model name" in line:
 			cpu = line
-	return render_template("settings.html", cpu=cpu)
+	return render_template("settings.html", cpu=cpu, memGig=memGig)
 	
 @app.route('/calc')
 def calc():
