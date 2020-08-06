c0921de2e3f54e9c2932f928ffe42f85b20cba01
mohamedhany1024 <mhany4485@gmail.com>
2020-08-06 13:32:46 +0200

    Home Screen Now Blurred On Power Options

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,9 @@ function power() {
     let bg = document.getElementById("menu");
     let power = document.getElementById("powerMenu");
     let body = document.getElementById("body");
-    bg.style.filter = ("blur(5px)");
+    let background = document.getElementById('backgroundHome');
+    bg.style.filter = ("blur(7px)");
+    background.style.filter = ("blur(7px)");
     power.style.display = ("block");
     power.style.zIndex = ("10");
 }
@@ -10,8 +12,10 @@ function power() {
 function exitPower() {
     let bg = document.getElementById("menu");
     let power = document.getElementById("powerMenu");
+    let background = document.getElementById('backgroundHome');
     power.style.display = ("none");
-    bg.style.filter = ("blur(0)")
+    bg.style.filter = ("blur(0)");
+    background.style.filter = ("blur(0)");
 }
 
 
